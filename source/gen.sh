8818165e3d1be808cf4ff96d087a03172470ed6f
Dmitriy Firsov <shtihlfd@yandex.ru>
2021-08-24 00:48:58 +0000

    regenerate site

---
+++ source/gen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+rm ../*.html 2>/dev/null || true
+rm -r ../author 2>/dev/null || true
+rm -r ../category 2>/dev/null || true
+rm -r ../rss 2>/dev/null || true
+rm -r ../theme 2>/dev/null || true
+
+pelican --relative-urls --ignore-cache \
+  -o .. content
+
+git add ../.
