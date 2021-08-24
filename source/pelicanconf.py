aba50e2361220f35e6b4e67c267b516aa60f0893
Dmitriy Firsov <shtihlfd@yandex.ru>
2021-08-24 01:07:36 +0000

    few fix

--- source/pelicanconf.py
+++ source/pelicanconf.py
@@ -30,6 +30,8 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
+SUMMARY_MAX_LENGTH = 20
+SUMMARY_END_SUFFIX = '…'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
